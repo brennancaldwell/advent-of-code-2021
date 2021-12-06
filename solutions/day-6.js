7de037f9792cd8243b958bedfb30f7047398a751
Brennan Caldwell <brennan.caldwell@gmail.com>
2021-12-06 12:16:28 -0500

    Part Two

--- solutions/day-6.js
+++ solutions/day-6.js
@@ -5,15 +5,15 @@ const fish = rawInput.split(',').map(x => Number(x))
 
 
 function partOne(array, repetitions) {
-  let fish = array, newBorns = 0, days = 0
+  let fish = array.slice(), newBorns = 0, days = 0
 
   while (days < repetitions) {
     for (let i = 0; i < fish.length; i++) {
-      if (array[i] === 0) {
-        array[i] = 6
+      if (fish[i] === 0) {
+        fish[i] = 6
         newBorns++
       } else {
-        array[i]--
+        fish[i]--
       }
     }
     while (newBorns > 0) {
@@ -27,3 +27,34 @@ function partOne(array, repetitions) {
 }
 
 console.log(partOne(fish, 80))
+
+function partTwo(array, repetitions) {
+  const fish = {
+    0: array.filter(val => val === 0).length,
+    1: array.filter(val => val === 1).length,
+    2: array.filter(val => val === 2).length,
+    3: array.filter(val => val === 3).length,
+    4: array.filter(val => val === 4).length,
+    5: array.filter(val => val === 5).length,
+    6: array.filter(val => val === 6).length,
+    7: array.filter(val => val === 7).length,
+    8: array.filter(val => val === 8).length
+  }
+
+  for (let i = 0; i < repetitions; i++) {
+    const zeroes = fish[0]
+    fish[0] = fish[1]
+    fish[1] = fish[2]
+    fish[2] = fish[3]
+    fish[3] = fish[4]
+    fish[4] = fish[5]
+    fish[5] = fish[6]
+    fish[6] = fish[7] + zeroes
+    fish[7] = fish[8]
+    fish[8] = zeroes
+  }
+
+  return Object.values(fish).reduce((a, b) => a + b)
+}
+
+console.log(partTwo(fish, 256))
