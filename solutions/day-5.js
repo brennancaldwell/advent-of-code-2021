e01163bcc2be4a8dfa8d5fa8bc9f821916cf328d
Brennan Caldwell <brennan.caldwell@gmail.com>
2021-12-05 19:07:51 -0500

    Part Two

--- solutions/day-5.js
+++ solutions/day-5.js
@@ -13,7 +13,7 @@ const partOneInput = array.filter(cp => {
 function partOne(arr) {
   const intersectionTracker = {}
 
-  partOneInput.forEach(pair => {
+  arr.forEach(pair => {
     const axis = pair[0].x === pair[1].x ? 'y' : 'x'
     let min = Math.min(pair[0][axis], pair[1][axis])
     const max = Math.max(pair[0][axis], pair[1][axis])
@@ -35,3 +35,71 @@ function partOne(arr) {
 }
 
 console.log(partOne(partOneInput))
+
+
+function partTwo(array) {
+  const intersectionTracker = {}
+
+  for (let i = 0; i < array.length; i++) {
+    let x1 = array[i][0].x,
+        x2 = array[i][1].x,
+        y1 = array[i][0].y,
+        y2 = array[i][1].y
+
+    if (x1 === x2 || y1 === y2) {
+      const axis = x1 === x2? 'y' : 'x'
+      let min = Math.min(array[i][0][axis], array[i][1][axis])
+      const max = Math.max(array[i][0][axis], array[i][1][axis])
+      while (min <= max) {
+        if (axis === 'x') {
+          intersectionTracker[`${min},${y1}`]
+          ? intersectionTracker[`${min},${y1}`]++
+          : intersectionTracker[`${min},${y1}`] = 1
+        } else {
+          intersectionTracker[`${x1},${min}`]
+          ? intersectionTracker[`${x1},${min}`]++
+          : intersectionTracker[`${x1},${min}`] = 1
+        }
+        min++
+      }
+    } else {
+      if (x1 < x2 && y1 < y2) {
+        while (x1 <= x2 && y1 <= y2) {
+          intersectionTracker[`${x1},${y1}`]
+          ? intersectionTracker[`${x1},${y1}`]++
+          : intersectionTracker[`${x1},${y1}`] = 1
+          x1++
+          y1++
+        }
+      } else if (x1 < x2 && y1 > y2) {
+        while (x1 <= x2 && y1 >= y2) {
+          intersectionTracker[`${x1},${y1}`]
+          ? intersectionTracker[`${x1},${y1}`]++
+          : intersectionTracker[`${x1},${y1}`] = 1
+          x1++
+          y1--
+        }
+      } else if (x1 > x2 && y1 < y2) {
+        while (x1 >= x2 && y1 <= y2) {
+          intersectionTracker[`${x1},${y1}`]
+          ? intersectionTracker[`${x1},${y1}`]++
+          : intersectionTracker[`${x1},${y1}`] = 1
+          x1--
+          y1++
+        }
+      } else {
+        while (x1 >= x2 && y1 >= y2) {
+          intersectionTracker[`${x1},${y1}`]
+          ? intersectionTracker[`${x1},${y1}`]++
+          : intersectionTracker[`${x1},${y1}`] = 1
+          x1--
+          y1--
+        }
+      }
+    }
+  }
+
+  return Object.values(intersectionTracker).filter(val => val >= 2).length
+}
+
+console.log(partTwo(array))
