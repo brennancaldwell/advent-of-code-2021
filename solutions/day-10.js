64da08f4c1e4354669591d18b37c7ba0143910c7
Brennan Caldwell <brennan.caldwell@gmail.com>
2021-12-10 12:36:53 -0500

    Part two

--- solutions/day-10.js
+++ solutions/day-10.js
@@ -10,15 +10,15 @@ const points = {
   '>': 25137
 }
 
-function partOne(array, points) {
-  let result = 0, tracker = []
+const end = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>'
+}
 
-  const end = {
-    '(': ')',
-    '[': ']',
-    '{': '}',
-    '<': '>'
-  }
+function partOne(array, points, end) {
+  let result = 0, tracker = []
 
   for (let i = 0; i < array.length; i++) {
     for (let x = 0; x < array[i].length; x++) {
@@ -45,4 +45,70 @@ function partOne(array, points) {
   return result
 }
 
-console.log(partOne(array, points))
+console.log(partOne(array, points, end))
+
+
+function corrupted(str, end) {
+
+  let tracker = [], result = false
+
+  for (let i = 0; i < str.length; i++) {
+    if (Object.keys(end).includes(str[i])) {
+      tracker.push(end[str[i]])
+    } else {
+      if (tracker.length === 0) {
+        result = true
+      } else {
+        if (tracker[tracker.length - 1] === str[i]) {
+          tracker.pop()
+        } else {
+          result = true
+        }
+      }
+    }
+    if (result) break
+  }
+
+  return result
+}
+
+const inputTwo = array.filter(line => !corrupted(line, end))
+
+function completeLine(str, end) {
+  let tracker = []
+
+  for (let i = 0; i < str.length; i++) {
+    if (Object.keys(end).includes(str[i])) {
+      tracker.push(end[str[i]])
+    } else {
+      tracker.pop()
+    }
+  }
+
+  return tracker.reverse().join('')
+}
+
+const pointsTwo = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4
+}
+
+function convertLineToScore(line, points) {
+  let result = 0
+
+  for (let i = 0; i < line.length; i++) {
+    result *= 5
+    result += points[line[i]]
+  }
+  return result
+}
+
+const pointsArray = inputTwo.map(line => convertLineToScore(completeLine(line, end), pointsTwo))
+
+function partTwo(arr) {
+  return arr.sort((a, b) => b - a)[Math.floor(arr.length / 2)]
+}
+
+console.log(partTwo(pointsArray))
